474b0d03e7c579efb7343a050ec67b5f75af0d96
Daniel Britt <28036018+djbritt@users.noreply.github.com>
2024-01-05 14:05:56 -0800

    Update contentScript.js
    enhanced html location check

--- contentScript.js
+++ contentScript.js
@@ -13,16 +13,45 @@ var num = 0;
 function printNum(){
   num++
   // console.log(num)
-  var currentTimeElement = document.evaluate('//*[@id="movie_player"]/div[32]/div[2]/div[1]/div[1]/span[2]/span[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  var durationElement = document.evaluate('//*[@id="movie_player"]/div[32]/div[2]/div[1]/div[1]/span[2]/span[3]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
-  if (!currentTimeElement) {
-    var currentTimeElement = document.evaluate('//*[@id="movie_player"]/div[33]/div[2]/div[1]/div[1]/span[2]/span[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  var currentTimeXpathExpressions = [
+      '//*[@id="movie_player"]/div[33]/div[2]/div[1]/div[1]/span[2]/span[1]',
+      '//*[@id="movie_player"]/div[32]/div[2]/div[1]/div[1]/span[2]/span[1]',
+      '//*[@id="movie_player"]/div[31]/div[2]/div[1]/div[1]/span[2]/span[1]'
+      // Add more XPath expressions here
+  ];
+
+  var currentTimeElement = null;
+
+  for (var i = 0; i < currentTimeXpathExpressions.length; i++) {
+      currentTimeElement = document.evaluate(currentTimeXpathExpressions[i], document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+      if (currentTimeElement) {
+          // If a valid currentTimeElement is found, break out of the loop
+          console.log('current time found')
+          break;
+      }
   }
 
-  if (!durationElement) {
-    var durationElement = document.evaluate('//*[@id="movie_player"]/div[33]/div[2]/div[1]/div[1]/span[2]/span[3]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  var currentTimeXpathExpressions = [
+      '//*[@id="movie_player"]/div[33]/div[2]/div[1]/div[1]/span[2]/span[3]',
+      '//*[@id="movie_player"]/div[32]/div[2]/div[1]/div[1]/span[2]/span[3]',
+      '//*[@id="movie_player"]/div[31]/div[2]/div[1]/div[1]/span[2]/span[3]'
+      // Add more XPath expressions here
+  ];
+
+  var durationElement = null;
+
+  for (var i = 0; i < currentTimeXpathExpressions.length; i++) {
+      durationElement = document.evaluate(currentTimeXpathExpressions[i], document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+      if (durationElement) {
+          // If a valid durationElement is found, break out of the loop
+          console.log('duration found')
+          break;
+      }
   }
+
 
   var currentTime = parseTime(currentTimeElement.innerText);
   var duration = parseTime(durationElement.innerText);
@@ -30,21 +59,30 @@ function printNum(){
   // console.log('currentTime', currentTime);
   // console.log('duration', duration);
   if (currentTime == duration) {
-    var nextButton = document.evaluate('//*[@id="movie_player"]/div[32]/div[2]/div[1]/a[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    // If div[32] elements are not found, try div[33]
-    if (!nextButton ) {
-      // console.log("nextButton 2")
-      nextButton = document.evaluate('//*[@id="movie_player"]/div[33]/div[2]/div[1]/a[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    } else if (!nextButton) {
-      // console.log("nextButton 3")
-      nextButton = document.evaluate('//*[@id="movie_player"]/div[33]/div[2]/div[1]/div[1]/span[2]/span[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    }
-    if (nextButton) {
-      // console.log(nextButton)
-      // $(nextButton)[0].click()
-      console.log("next button clicked");
-      nextButton.click();
+    console.log("end of video reached")
+
+    var currentTimeXpathExpressions = [
+        '//*[@id="movie_player"]/div[32]/div[2]/div[1]/a[2]',
+        '//*[@id="movie_player"]/div[33]/div[2]/div[1]/a[2]',
+        '//*[@id="movie_player"]/div[33]/div[2]/div[1]/div[1]/span[2]/span[1]',
+        '//*[@id="movie_player"]/div[31]/div[2]/div[1]/a[2]'
+        // Add more XPath expressions here
+    ];
+
+    var nextButton = null;
+
+    for (var i = 0; i < currentTimeXpathExpressions.length; i++) {
+        nextButton = document.evaluate(currentTimeXpathExpressions[i], document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+        if (nextButton) {
+            // If a valid durationElement is found, break out of the loop
+            console.log('next button found')
+            console.log("next button clicked");
+            nextButton.click();
+            break;
+        }
     }
   }
+
 }
 setInterval(printNum, 1000);
